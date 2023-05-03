41a430adfe3b247e00fac41b5cd267cc58c70bec
helinz1 <130932031+helinz1@users.noreply.github.com>
2023-05-03 23:35:31 +0300

    add weather background images

--- app.js
+++ app.js
@@ -4,3 +4,38 @@ window.onload = () => {
         .then(data => console.log(`Client IP Address: ${data.ip}`))
         .catch(error => console.error('Error fetching IP address:', error));
 };
+
+
+let currentBackgroundImage = document.getElementsByTagName('main')[0].classList[0];
+
+const backgroundImageClasses = {
+    sunny: 'weather-sunny-img', 
+    cloudy: 'weather-cloudy-img', 
+    rainy: 'weather-rainy-img',
+    snowy: 'weather-snowy-img'
+};
+
+
+
+// Example logging 
+// let weatherConditions = 'snowy';
+
+
+const handleBackgroundImage = (weatherConditions) => {
+    if (weatherConditions === 'sunny') {
+        currentBackgroundImage = backgroundImageClasses.sunny;
+    };
+    
+    if (weatherConditions === 'cloudy') {
+        currentBackgroundImage = backgroundImageClasses.cloudy;
+    };
+    
+    if (weatherConditions === 'rainy') {
+        currentBackgroundImage = backgroundImageClasses.rainy;
+    };
+    
+    if (weatherConditions === 'snowy') {
+        currentBackgroundImage = backgroundImageClasses.snowy;
+    };
+    document.getElementsByTagName('main')[0].className = currentBackgroundImage;
+};
