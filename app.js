3ef620f055de0b8a8197414754feb7514ea1240e
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2023-06-10 20:43:01 -0700

    Integrate weather service api

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// Geolocation elements
-const cityElement = document.getElementById('city');
-const regionElement = document.getElementById('region');
-const countryElement = document.getElementById('country');
+const apiUrl = 'https://weather-service-vbnet.azurewebsites.net/api/currentweather';
 
-let clientIpAddress;
-let geolocation;
+const el = id => document.getElementById(id);
+const headlineElement = el('headline');
+const geolocationElement = el('geolocation');
+const temperatureElement = el('temperature');
+const humidityElement = el('humidity');
+const windElement = el('wind');
+const mainElement = document.getElementsByTagName('main')[0];
 
-let currentBackgroundImage = document.getElementsByTagName('main')[0].classList[0];
 const backgroundImageClasses = {
     sunny: 'weather-sunny-img',
     cloudy: 'weather-cloudy-img',
@@ -14,86 +15,40 @@ const backgroundImageClasses = {
     snowy: 'weather-snowy-img'
 };
 
-// Weather elements
-const temperatureElement = document.getElementById('temperature');
-
-// Get client IP address
-window.onload = () => {
-    fetch('https://api.ipify.org/?format=json')
-        .then(response => response.json())
-        .then(data => {
-            console.log(`Client IP Address: ${data.ip}`);
-            clientIpAddress = data.ip;
-
-            try {
-                getGeolocation(clientIpAddress)
-                    .then(data => {
-                        console.log(`Geolocation: ${data.data.city}, ${data.data.region}, ${data.data.country}`);
-                        setGeolocation(data.data);
-
-                        // TESTING
-                        setWeatherDetails();
-
-                    })
-                    .catch(error => console.error('Error getting geolocation:', error));
-            }
-            catch (error) {
-                console.error('Error getting geolocation:', error);
-            }
-        })
-        .catch(error => console.error('Error fetching IP address:', error));
+const headlines = {
+    sunny: "It's sunny today",
+    cloudy: "It's cloudy today",
+    rainy: "It's rainy today",
+    snowy: "It's snowy today"
 };
 
-// API Post request to get geolocation
-const getGeolocation = async (clientIpAddress) => {
-    const response = await fetch('http://127.0.0.1:8000/api/geolocation', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            ip_address: clientIpAddress
-        })
-    });
+const handleBackgroundImage = (weatherCondition) => {
+    weatherCondition = weatherCondition.toLowerCase();
+    mainElement.className = backgroundImageClasses[weatherCondition];
+    headlineElement.innerText = headlines[weatherCondition];
+};
 
-    if (!response.ok) {
-        throw new Error(`Error getting geolocation: ${response.statusText}`);
+const fetchWeatherData = async () => {
+    try {
+        const response = await fetch(apiUrl);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching API Response:', error);
     }
-
-    const data = await response.json();
-    return data;
 };
 
-// Set geolocation on page
-const setGeolocation = (geolocation) => {
-    cityElement.innerText = geolocation.city;
-    regionElement.innerText = geolocation.region;
-    countryElement.innerText = geolocation.country;
+const updateUI = (data) => {
+    console.log(`API Response: ${JSON.stringify(data)}`);
+
+    geolocationElement.innerText = data.Geolocation;
+    temperatureElement.innerText = `${data.TemperatureC} °C / ${data.TemperatureF}  °F`;
+    humidityElement.innerText = `Humidity: ${data.Humidity}`
+    windElement.innerText = `Wind: ${data.WindKph} KPH / ${data.WindMph} MPH`
+
+    handleBackgroundImage(data.Condition);
 };
 
-
-// TESTING
-// Set weather details on page
-const setWeatherDetails = () => {
-    temperatureElement.innerText = '20 °C / 68 °F';
-};
-
-
-const handleBackgroundImage = (weatherConditions) => {
-    if (weatherConditions === 'sunny') {
-        currentBackgroundImage = backgroundImageClasses.sunny;
-    };
-
-    if (weatherConditions === 'cloudy') {
-        currentBackgroundImage = backgroundImageClasses.cloudy;
-    };
-
-    if (weatherConditions === 'rainy') {
-        currentBackgroundImage = backgroundImageClasses.rainy;
-    };
-
-    if (weatherConditions === 'snowy') {
-        currentBackgroundImage = backgroundImageClasses.snowy;
-    };
-    document.getElementsByTagName('main')[0].className = currentBackgroundImage;
+window.onload = async () => {
+    const weatherData = await fetchWeatherData();
+    updateUI(weatherData);
 };
